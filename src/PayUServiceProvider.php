bffd0df23346e992937fc5c9251de6c0adc7ea85
Bolebor <korotm@gmail.com>
2020-03-08 20:22:19 +0100

    first method

--- src/PayUServiceProvider.php
+++ src/PayUServiceProvider.php
@@ -4,9 +4,21 @@ namespace Korotkiewicz\PayU;
 
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
+use \Illuminate\Contracts\Foundation\Application;
 
 
 class PayUServiceProvider extends ServiceProvider {
+
+    /**
+     * Create a new service provider instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @return void
+     */
+    public function __construct(Application $app)
+    {
+        parent::__construct($app);
+    }
 
     /**
      * Indicates if loading of the provider is deferred.
